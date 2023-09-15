713e014c6d3798ac1b912d85a01fc67e2807d422
Roman <36523168+romanazimov@users.noreply.github.com>
2023-09-15 11:29:48 -0400

    Updated component implementation

--- src/sections/Advantages.tsx
+++ src/sections/Advantages.tsx
@@ -1,5 +1,5 @@
 import './Advantages.scss'
-import Reason from '../components/Advantages/Reason'
+import ReasonComp from '../components/Advantages/Reason'
 import line from '../assets/advantages/icon_line.png'
 import equipment from '../assets/advantages/icon_equipment.png'
 import zones from '../assets/advantages/icon_fitness_zones.png'
@@ -11,19 +11,19 @@ export default function Advantages() {
             <div className='container'>
                 <h1>Reasons to Join</h1>
                 <div className='reasons'>
-                    <Reason
+                    <ReasonComp
                         img={line}
                         info={{ title: '45000 SQ.FT.', description: 'A spacious gym for various sports - a safe distance between all exercise machines'}}
                     />
-                    <Reason
+                    <ReasonComp
                         img={equipment}
                         info={{ title: 'TONS OF GYM EQUIPMENT', description: 'Premium gym equipment from LifeStyle, Hammer Strength, and TechnoGym'}}
                     />
-                    <Reason
+                    <ReasonComp
                         img={zones}
                         info={{ title: '4 FITNESS ZONES', description: 'From cardio to functional strength training along with a seperate area for boxing and mma'}}
                     />
-                    <Reason
+                    <ReasonComp
                         img={time}
                         info={{ title: '24/7 GYM OPERATION', description: 'We are open 24 hours a day including all holidays'}}
                     />
