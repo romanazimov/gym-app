1914f86400ae913b73976afcbd82eb7cbf07bb1f
Roman <36523168+romanazimov@users.noreply.github.com>
2023-09-15 11:52:20 -0400

    Cleaned up and renamed folder

--- src/sections/Advantages.tsx
+++ src/sections/Advantages.tsx
@@ -1,5 +1,5 @@
 import './Advantages.scss'
-import Reason from '../components/Advantage/Reason'
+import Reason from '../components/Advantages/Reason'
 import line from '../assets/advantages/icon_line.png'
 import equipment from '../assets/advantages/icon_equipment.png'
 import zones from '../assets/advantages/icon_fitness_zones.png'
